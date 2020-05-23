39f8b8df2429ea1c248449d681b72d0d71c1a0c2
Sean Worthington <sean@worthington.net>
2020-05-22 22:22:18 -0700

    Update ddns.php

--- SkyWallet Protocol/DDNS/ddns.php
+++ SkyWallet Protocol/DDNS/ddns.php
@@ -21,10 +21,10 @@ $trustedDomainName = "cloudcoin.global";
 $date = date("Y-m-d H:i:s");
 
  /* Cloudflare.com | APİv4 | Api Ayarları */
-$apikey = 'ce5ec1e3bGet From Seanb3d46e9d107f6'; // Cloudflare Global API
-$email = 'YourEmail@nmail.com'; // Cloudflare Email Adress
-$domain = 'skywallet.cc';  // zone_name // Cloudflare Domain Name
-$zoneid = '561ed8e8Get from sean16fd2cdc234'; // zone_id // Cloudflare Domain Zone ID
+$apikey = 'ce5eYour CLoudFlare API here 6e9d107f6'; // Cloudflare Global API
+$email = 'youremailnmail.com'; // Cloudflare Email Adress
+$domain = 'yourdomain.com';  // zone_name // Cloudflare Domain Name
+$zoneid = '561ed8e your zone id 2cdc234'; // zone_id // Cloudflare Domain Zone ID
 
 
 //SEE IF GET VARIABLES ARE NOT THERE
@@ -131,7 +131,7 @@ $dnsipgeldi = long2ip( $sn );//String will now start with
 /* SEE IF IP ADDRESS IS ALREAYD IN THE DNS */
 //Do not allow the same account to have two ips. 
 
-/*
+
  $get = curl_init("https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&content=$dnsipgeldi&match=all");
 			curl_setopt($get, CURLOPT_RETURNTRANSFER, true);
     		curl_setopt($get, CURLOPT_FOLLOWLOCATION, true);
@@ -148,19 +148,21 @@ $dnsipgeldi = long2ip( $sn );//String will now start with
         $return = curl_exec($get);
 	
         curl_close($get);
-   
+ 
 		// See if the IP address is already used. 
-        if( strpos($return, 'count": 0') !== false )
+        if( contains($return, 'count": 0') == "false" || contains($return, 'count":0') == "false"  )
 		{
-            //There is no record for that ip. 
-           // echo "A record was found" .$return;
-        }else{
-          // echo "here" .$return;
-            die('{"server":"' . $thisServerName . '","status":"fail","message":"IP address already has a DNS name.","time":"'.$date.'"}');    
+		   //The number of DNS recoreds that match are zero
+            
+		}
+		else
+		{
+		    die('{"server":"' . $thisServerName . '","status":"fail","message":"IP address already has a DNS name.","time":"'.$date.'"}');    
 		}
 		
+   /*   */
     //echo "Your ip is not used";
-*/
+
     // A-record oluşturur DNS sistemi için.
     		$ch = curl_init("https://api.cloudflare.com/client/v4/zones/".$zoneid."/dns_records");
     		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -192,15 +194,26 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 //curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data_string));
 
 $sonuc = curl_exec($ch);
- echo "Sonuc is ".$sonuc;
-// If you want show output remove code slash.
-if (strpos($sonuc, 'success": true') !== false) {
+ //echo "Sonuc is ".$sonuc;
+
+
+//These CloudFlare fuckers keep changing the protcol by adding a space or changing the word. Check for all
+if ( strpos($sonuc, 'status":true') !== false) {
     die( '{"status":"success","errors":[],"messages":[],"result":"' . $username . ' created"}');
 }
 
 if (strpos($sonuc, 'status": true') !== false) {
     die ('{"status":"success","errors":[],"messages":[],"result":"' . $username . ' created"}');
 }
+
+if (strpos($sonuc, 'success": true') !== false) {
+    die ('{"status":"success","errors":[],"messages":[],"result":"' . $username . ' created"}');
+}
+
+if (strpos($sonuc, 'success":true') !== false) {
+    die ('{"status":"success","errors":[],"messages":[],"result":"' . $username . ' created"}');
+}
+
 
     $sonuc = str_replace("success", "status", $sonuc);
     $sonuc = str_replace("false", "\"fail\"", $sonuc);
@@ -230,5 +243,15 @@ function is_valid_domain_name($domain_name)
             && preg_match("/^.{1,253}$/", $domain_name) //overall length check
             && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
 }
+
+function contains( $haystack, $needle )
+{
+     if ( strpos( $haystack, $needle ) !== false ) 
+    {
+        return 'true';
+    }
+        return 'false';
+}//end contains
+
 
 ?>
